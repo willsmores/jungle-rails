122fc61c0f609444ac38fe521c0467ed3833f4ae
Steve Willsmore <scwillsmore@gmail.com>
2023-01-25 20:47:06 -0500

    Add routing for about page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
-  get 'about/index'
   root to: 'products#index'
+
+  get 'about' => 'about#index'
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
