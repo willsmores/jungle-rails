8315cab245153e8a2869bd60b3d2999238c5de4a
Steve Willsmore <scwillsmore@gmail.com>
2023-01-31 16:43:41 -0500

    Reword tests to be clearer.

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,13 +1,13 @@
-describe("Navigation", () => {
+describe("Navigation to Home Page", () => {
   it("should visit root", () => {
     cy.visit("/");
   });
 
-  it("There is products on the page", () => {
+  it("should show products on the page", () => {
     cy.get(".products article").should("be.visible");
   });
 
-  it("There is 2 products on the page", () => {
+  it("should show 2 products on the page", () => {
     cy.get(".products article").should("have.length", 2);
   });
 });
