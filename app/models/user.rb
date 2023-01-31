ef89fee7ecc37f11569f25f1482e77febd51a031
Steve Willsmore <scwillsmore@gmail.com>
2023-01-31 13:46:46 -0500

    Add support for incorrect email case and whitespace.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,19 @@
 class User < ApplicationRecord
 
+  def authenticate_with_credentials(email, password)
+    stripped_email = email.strip
+    
+    @user = User.find_by_email(stripped_email.downcase)
+
+    if @user && @user.authenticate(password)
+      @user
+    else
+      nil
+    end
+  end
+
   has_secure_password
 
-  validates :email, uniqueness: true
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, :uniqueness => { :case_sensitive => false }
