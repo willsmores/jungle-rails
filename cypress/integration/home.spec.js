5a83ffa880c3d3b85eaf69925e83b3cb5c790f72
Steve Willsmore <scwillsmore@gmail.com>
2023-01-31 16:29:55 -0500

    Add test to check for multiple products.

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,9 +1,13 @@
 describe("Navigation", () => {
   it("should visit root", () => {
-    cy.visit("http://localhost:3000/");
+    cy.visit("/");
   });
 
   it("There is products on the page", () => {
     cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
   });
 });
